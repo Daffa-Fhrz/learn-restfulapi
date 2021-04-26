54e9f73b0d562049c7b9e82bfadbbf2ce5fcca42
Daffa-Fhrz <fadaf793@gmail.com>
2021-04-26 15:27:02 +0700

    create produtc routes

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-const express = require('express');
-const knex = require('knex')
-require('dotenv').config();
+const express = require("express");
+const knex = require("knex");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // connection
-const knexfile = require('./knexfile');
-const NODE_ENV = process.env.NODE_ENV || 'development';
+const knexfile = require("./knexfile");
+const NODE_ENV = process.env.NODE_ENV || "development";
 
 knex(knexfile[NODE_ENV]);
-console.log(process.env.DATABASE);
+
+// router
+// import router
+const product = require("./routes/product");
+
+app.use('/api/product', product);
 
 // listening the server
 app.listen(PORT, () => {
 	console.log(`listening on port ${PORT}`);
-})
+});
--- routes/product.js
+++ routes/product.js
@@ -2,6 +2,9 @@ const { Router } = require('express');
 
 const router = Router();
 
-
+router.get('/');
+router.get('/:id');
+router.post('/');
+router.put('/:id');
 
 module.exports = router;
