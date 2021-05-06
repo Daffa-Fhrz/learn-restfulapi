4fd1d2f35e816acfa22fb9709abdfdcc1fa8f192
Daffa-Fhrz <fadaf793@gmail.com>
2021-05-06 13:54:11 +0700

    make update function in product controller

--- routes/product.route.js
+++ routes/product.route.js
@@ -2,11 +2,11 @@ const { Router } = require('express');
 
 const router = Router();
 
-const { create, getAll, getOne} = require("../controllers/product.controller");
+const { create, getAll, getOne, update } = require("../controllers/product.controller");
 
 router.get('/', getAll);
 router.get('/:id', getOne);
 router.post('/', create);
-router.put('/:id');
+router.put('/:id', update);
 
 module.exports = router;
